0e3e556a5e65280d9aa8cc0dadc9e0f60296f8ad
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-22 20:53:03 +0100

    calculateAgeOnPlanet method refactored to use orbital years in enum class

--- src/main/java/SpaceCalculator.java
+++ src/main/java/SpaceCalculator.java
@@ -10,30 +10,33 @@ public class SpaceCalculator {
 //    Neptune: Orbital period 164.79132 Earth years
 //    Pluto: Orbital period 248.00 Earth years
 
-
     public static double calculateAgeOnPlanet(Planet planet, double age) {
-        double ageOnPlanet = 0;
-        if (planet == Planet.EARTH){
-            ageOnPlanet = age;
-        } else if (planet == Planet.MERCURY){
-            ageOnPlanet = (age / 0.2408467);
-        } else if (planet == Planet.VENUS){
-            ageOnPlanet = (age / 0.61519726);
-        } else if (planet == Planet.MARS){
-            ageOnPlanet = (age / 1.8808158);
-        } else if (planet == Planet.JUPITER){
-            ageOnPlanet = (age / 11.862615);
-        } else if (planet == Planet.SATURN){
-            ageOnPlanet = (age / 29.447498);
-        } else if (planet == Planet.URANUS){
-            ageOnPlanet = (age / 84.016846);
-        } else if (planet == Planet.NEPTUNE){
-            ageOnPlanet = (age / 164.79132);
-        } else if (planet == Planet.PLUTO){
-            ageOnPlanet = (age / 248.00);
-        }
-        return ageOnPlanet;
+        return age / planet.getOrbitalPeriod();
     }
+
+//    public static double calculateAgeOnPlanet(Planet planet, double age) {
+//        double ageOnPlanet = 0;
+//        if (planet == Planet.EARTH){
+//            ageOnPlanet = age;
+//        } else if (planet == Planet.MERCURY){
+//            ageOnPlanet = (age / 0.2408467);
+//        } else if (planet == Planet.VENUS){
+//            ageOnPlanet = (age / 0.61519726);
+//        } else if (planet == Planet.MARS){
+//            ageOnPlanet = (age / 1.8808158);
+//        } else if (planet == Planet.JUPITER){
+//            ageOnPlanet = (age / 11.862615);
+//        } else if (planet == Planet.SATURN){
+//            ageOnPlanet = (age / 29.447498);
+//        } else if (planet == Planet.URANUS){
+//            ageOnPlanet = (age / 84.016846);
+//        } else if (planet == Planet.NEPTUNE){
+//            ageOnPlanet = (age / 164.79132);
+//        } else if (planet == Planet.PLUTO){
+//            ageOnPlanet = (age / 248.00);
+//        }
+//        return ageOnPlanet;
+//    }
 
 
     public static double calculateAgeOnPlanetInSeconds(Planet planet, int age) {
