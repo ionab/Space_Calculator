024a7b073e924f31ce6d03b23c2b516c4f4099a8
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-22 20:50:54 +0100

    planet enum refactored to include orbitalPeriod

--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -1,11 +1,21 @@
 public enum Planet {
-    EARTH,
-    MERCURY,
-    VENUS,
-    MARS,
-    JUPITER,
-    SATURN,
-    URANUS,
-    NEPTUNE,
-    PLUTO
+    EARTH(1),
+    MERCURY(0.2408467),
+    VENUS(0.61519726),
+    MARS(1.8808158),
+    JUPITER(11.862615),
+    SATURN(29.447498),
+    URANUS(84.016846),
+    NEPTUNE(164.79132),
+    PLUTO(248.00);
+
+    private final double orbitalPeriod;
+
+    Planet(double orbitalPeriod) {
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
 }
