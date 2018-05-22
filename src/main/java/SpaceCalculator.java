db155f2526960001e3d5b97c2791b1423f55b6c7
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-22 20:14:49 +0100

    adds to calculateAgeOnPlanetMethod for Mercury

--- src/main/java/SpaceCalculator.java
+++ src/main/java/SpaceCalculator.java
@@ -1,10 +1,23 @@
 public class SpaceCalculator {
 
+//    Earth: Orbital period 365.25 Earth days, or 31,557,600 seconds
+//    Mercury: Orbital period 0.2408467 Earth years
+//    Venus: Orbital period 0.61519726 Earth years
+//    Mars: Orbital period 1.8808158 Earth years
+//    Jupiter: Orbital period 11.862615 Earth years
+//    Saturn: Orbital period 29.447498 Earth years
+//    Uranus: Orbital period 84.016846 Earth years
+//    Neptune: Orbital period 164.79132 Earth years
+//    And, depending on your feelings towards Pluto...
+//    Pluto: Orbital period 248.00 Earth years
 
-    public static double calculateAgeOnPlanet(Planet planet, int age) {
+
+    public static double calculateAgeOnPlanet(Planet planet, double age) {
         double ageOnPlanet = 0;
         if (planet == Planet.EARTH){
             ageOnPlanet = age;
+        } else if (planet == Planet.MERCURY){
+            ageOnPlanet = (age / 0.2408467);
         }
         return ageOnPlanet;
     }
