44b555503ea3a874022defdbe7bdb22f6368c92e
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-22 19:37:51 +0100

    adds test for calculating age on mercury

--- src/test/java/SpaceCalculatorTest.java
+++ src/test/java/SpaceCalculatorTest.java
@@ -7,11 +7,19 @@ public class SpaceCalculatorTest {
 
 //    earth, mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto
 
+// NB model assumes the age is exactly 27.
+
 // Test using org.unit that when you use the method calculateAgeOnPlanet method, passing in the arguments of
-//  the planet Enum earth, you get 27 back, with a delta value of 0.01.
+//  the planet Enum earth & the age 27, you get 27 back, with a delta value of 0.01.
     @Test
     public static void canGetAgeOnEarth(){
         assertEquals(27, SpaceCalculator.calculateAgeOnPlanet(Planet.EARTH, 27), 0.01);
+    }
+// Test using org.unit that when you use the method calculateAgeOnPlanet method, passing in the arguments of
+//  the planet Enum Mercury & the age 27, you get 112.10450465 back, with a delta value of 0.01.
+    @Test
+    public static void canGetAgeOnMercury(){
+        assertEquals(112.10450465, SpaceCalculator.calculateAgeOnPlanet(Planet.MERCURY, 27), 0.01);
     }
 
 }
